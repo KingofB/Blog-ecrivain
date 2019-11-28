bd054f72bec32d4f5e6b31cd2e47449fe90dce76
David Blanchet <blanchet.bzh@gmail.com>
2019-11-28 17:43:24 +0100

    Ajout d'images, modif page accueil

--- public/sql/tables.sql
+++ public/sql/tables.sql
@@ -66,4 +66,31 @@ CREATE UNIQUE INDEX ind_unique_pseudo ON Member(pseudo);
 CREATE INDEX ind_com_date ON Comment(comment_date);
 
 
+-- Insertion de membres dans la table Membre :
+
+INSERT INTO Member (pseudo, email, passw)
+VALUES ('Jean', 'jf@gmail.com', 'Jean'),
+        ('Aude', 'a@gmail.com', 'Aude'),
+        ('Vanessa', 'v@gmail.com', 'Vanessa'),
+        ('David', 'blanchet.bzh@gmail.com', 'Op04Er08Ki16');
+
+
+-- Insertion dans la table Article :
+
+INSERT INTO Article (title, image, summary, content, author_id, publication_date)
+VALUES ('Chapitre 1', 'man-in-airport.jpg', 'Un billet simple c\'est à la fois un début et une fin.',
+'Martinus agens illas provincias pro praefectis aerumnas innocentium graviter gemens saepeque obsecrans, ut ab omni culpa inmunibus parceretur, cum non inpetraret, minabatur se
+discessurum: ut saltem id metuens perquisitor malivolus tandem desineret quieti coalitos homines in aperta pericula proiectare.
+Et licet quocumque oculos flexeris feminas adfatim multas spectare cirratas, quibus, si nupsissent, per aetatem ter iam nixus poterat suppetere liberorum, ad usque taedium pedibus
+pavimenta tergentes iactari volucriter gyris, dum exprimunt innumera simulacra, quae finxere fabulae theatrales.
+Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.', 1, '2019-11-11 09:00:00'),
+        ('Chapitre 2', 'welcome.jpg', 'Arrivée à Fairbanks le 13 spetembre, 4°c.',
+'Martinus agens illas provincias pro praefectis aerumnas innocentium graviter gemens saepeque obsecrans, ut ab omni culpa inmunibus parceretur, cum non inpetraret, minabatur se
+discessurum: ut saltem id metuens perquisitor malivolus tandem desineret quieti coalitos homines in aperta pericula proiectare.
+Et licet quocumque oculos flexeris feminas adfatim multas spectare cirratas, quibus, si nupsissent, per aetatem ter iam nixus poterat suppetere liberorum, ad usque taedium pedibus
+pavimenta tergentes iactari volucriter gyris, dum exprimunt innumera simulacra, quae finxere fabulae theatrales.
+Fieri, inquam, Triari, nullo pacto potest, ut non dicas, quid non probes eius, a quo dissentias. quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum praesertim illa perdiscere ludus esset. Quam ob rem dissentientium inter se reprehensiones non sunt vituperandae, maledicta, contumeliae, tum iracundiae, contentiones concertationesque in disputando pertinaces indignae philosophia mihi videri solent.', 1, '2019-11-18 10:00:00'),
+        (),
+
+
 
