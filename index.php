20f8c05337972d994b7aa52da42a4950bb6e618c
David Blanchet <blanchet.bzh@gmail.com>
2019-12-18 17:50:44 +0100

    reduction des niveaux de conditions du routeur index.php

--- index.php
+++ index.php
@@ -3,24 +3,34 @@ require('controller/controller.php');
 
 if (isset($_GET['action'])) {
     if ($_GET['action'] == 'article') {
-        if (isset($_GET['article_id']) && $_GET['article_id'] > 0) {
-            article();
-        } else {
-            echo 'Erreur : aucun identifiant d\'article envoyé !';
-        }
+        articleController();
     } elseif ($_GET['action'] == 'home') {
         home();
     } elseif ($_GET['action'] == 'addComment') {
-        if (isset($_GET['article_id']) && is_numeric($_GET['article_id']) > 0) {
-            if (!empty($_POST['pseudo']) && !empty($_POST['content'])) {
-                comment($_GET['article_id'], $_POST['pseudo'], $_POST['content']);
-            } else {
-                echo 'Tous les champs doivent être remplis.';
-            }
-        } else {
-            echo 'Erreur : aucun identifiant d\'article envoyé !';
-        }
+        addCommentController();
     }
 } else {
     home();
+}
+
+function articleController()
+{
+    if (isset($_GET['article_id']) && $_GET['article_id'] > 0) {
+        article();
+    } else {
+        echo 'Erreur : aucun identifiant d\'article envoyé !';
+    }
+}
+
+function addCommentController()
+{
+    if (isset($_GET['article_id']) && is_numeric($_GET['article_id']) > 0) {
+        if (!empty($_POST['pseudo']) && !empty($_POST['content'])) {
+            comment($_GET['article_id'], $_POST['pseudo'], $_POST['content']);
+        } else {
+            echo 'Tous les champs doivent être remplis.';
+        }
+    } else {
+        echo 'Erreur : aucun identifiant d\'article envoyé !';
+    }
 }
