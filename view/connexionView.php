94833c1296dd2e71fbe0e0392c98e651b2d46420
David Blanchet <blanchet.bzh@gmail.com>
2019-12-20 23:58:35 +0100

    connexionView

--- view/connexionView.php
+++ view/connexionView.php
@@ -15,19 +15,19 @@ ob_start();
         <form class="col-md-6 pt-5" action="/proj4/index.php?action=connexion" method="POST">
             <div class="form-group">
                 <label for="exampleInputEmail1">Pseudo</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrer pseudo">
+                <input type="text" name="pseudo" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrer pseudo">
             </div>
             <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrer email">
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrer email">
                 <small id="emailHelp" class="form-text text-muted">Votre email, ainsi que toutes vos données personnelles, ne seront divulgués à aucune autre société ou site.</small>
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
             </div>
             <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <input type="checkbox" class="form-check-input" name="checkBoxRGPD" id="exampleCheck1">
                 <label class="form-check-label" for="exampleCheck1">J'accepte les règles de confidentialité et d'utilisation de ce site.</label>
             </div>
             <button type="submit" id="newMember" class="btn btn-primary">Valider</button>
